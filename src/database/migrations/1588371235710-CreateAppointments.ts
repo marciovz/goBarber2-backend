a0da293de809153c6ff0f7818cbb3f160e6edaf5
marciovz <marciovz@yahoo.com.br>
2020-05-02 21:50:10 -0300

    Adicionado os campos created_at e updated_at no modelo e tabela de appointments

--- src/database/migrations/1588371235710-CreateAppointments.ts
+++ src/database/migrations/1588371235710-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1588371235710
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
