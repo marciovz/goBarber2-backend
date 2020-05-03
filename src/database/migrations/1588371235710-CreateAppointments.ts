9c02c8129971922031fb72e73b8ba679498715e0
marciovz <marciovz@yahoo.com.br>
2020-05-02 22:48:57 -0300

    Implementado relacionamento no modelo de appointments

--- src/database/migrations/1588371235710-CreateAppointments.ts
+++ src/database/migrations/1588371235710-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588371235710
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1588465393811-CreateUsers.ts
+++ src/database/migrations/1588465393811-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588465393811 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
