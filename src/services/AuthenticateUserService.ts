38e38424c5156c613120ad600d57ce7e1e3bc137
marciovz <marciovz@yahoo.com.br>
2020-05-03 06:27:17 -0300

    Implementado a geração do token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
-import { hash, compare } from 'bcryptjs';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,7 +32,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, 'chave_secreta_md5', {
+      subject: user.id,
+      expiresIn: '7d',
+    });
+
+    return { user, token };
   }
 }
 
