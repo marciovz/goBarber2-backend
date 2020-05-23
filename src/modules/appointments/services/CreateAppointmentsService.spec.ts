5d47cc03e31b9aaae77443600f6fef77f18308dd
marciovz <marciovz@yahoo.com.br>
2020-05-23 12:48:19 -0300

    Criando repositório fake e primeiro teste de appointments

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,23 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/fakeAppointmentsREpository';
+import CreateAppointmentService from './CreateAppointmentSevice';
+
+describe('CreateAppointment', () => {
+  it('should be able to crate a new appointment', async () => {
+    const fakeAppointmentsREpository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsREpository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('shoukd not be to crate two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
