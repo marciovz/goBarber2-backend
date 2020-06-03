da1a8b0b594d25dc2232bd5564814c8811f0bb94
marciovz <marciovz@yahoo.com.br>
2020-06-03 14:55:31 -0300

    Implementado validação dos dados de requisição nas rotas com a biblioteca selebrate

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -13,7 +14,23 @@ const providersDayAvailabilityController = new ProvidersDayAvailabilityControlle
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
-providersRouter.get('/:provider_id/month-availability', providersMonthAvailabilityController.index);
-providersRouter.get('/:provider_id/day-availability', providersDayAvailabilityController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    }
+  }),
+  providersMonthAvailabilityController.index
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    }
+  }),
+  providersDayAvailabilityController.index
+);
 
 export default providersRouter;
