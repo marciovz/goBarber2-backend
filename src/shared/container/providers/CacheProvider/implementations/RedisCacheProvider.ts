ce8a1de51c1e4e9cbd241c381e01467530302edc
marciovz <marciovz@yahoo.com.br>
2020-06-05 20:42:50 -0300

    Invalidando o cache ao adicionar um novo user

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -27,4 +27,16 @@ export default class RedisCacheProvider implements ICacheProvider {
     throw new Error("Method not implemented.");
   }
 
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
+
 }
